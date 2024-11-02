ff84d9dbfadf6712b1e77ad5341ae277619a8832
ysadamt <adam.teo321@gmail.com>
2024-11-02 17:29:28 -0500

    redeploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import { signIn, signOut } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   if (session) {
     return (
       <div className='p-6'>
